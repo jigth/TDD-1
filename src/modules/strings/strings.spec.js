1006c259be115d5295b2948006d8e57d964e6ab9
dochoamontes <danielochoamontes@gmail.com>
2024-06-02 00:01:41 -0500

    REFACTOR: DRY strings spec

--- src/modules/strings/strings.spec.js
+++ src/modules/strings/strings.spec.js
@@ -1,17 +1,20 @@
 const { Strings } = require('./strings')
 describe('Strings', () => {
+    let strings
+    
+    beforeEach(() => {
+        strings = new Strings()
+    })
+
     it('test if "mom" is a palyndrome', () => {
-        const strings = new Strings()
         expect(strings.isPalyndrome('mom')).toBeTruthy()
     })
 
     it('test if "osossoso" is a palyndrome', () => {
-        const strings = new Strings()
         expect(strings.isPalyndrome('osossoso')).toBeTruthy()
     })
 
     it('test if "Daniel" is a palyndrome', () => {
-        const strings = new Strings()
         expect(strings.isPalyndrome('Daniel')).toBeFalsy()
     })
 })
