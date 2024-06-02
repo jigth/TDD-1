5ce8404aabf1241c4b0ff6aed410f5c151f96575
dochoamontes <danielochoamontes@gmail.com>
2024-06-01 23:37:37 -0500

    REFACTOR: Simplify arithmetic class methods

--- src/modules/math/arithmetic.js
+++ src/modules/math/arithmetic.js
@@ -1,21 +1,21 @@
 class Arithmetic {
-    sum = (a, b) => {
+    sum (a, b) {
         return a + b;
     }
     
-    mult = (a, b) => {
+    mult (a, b) {
         return a * b
     }
 
-    sub = (a, b) => {
+    sub (a, b) {
         return a - b
     }
 
-    div = (a, b) => {
+    div (a, b) {
         return a / b
     }
 
-    fact = (n) => {
+    fact (n) {
         if (n == 1 | n == 0) return 1
         return n * this.fact(n-1)
     }
