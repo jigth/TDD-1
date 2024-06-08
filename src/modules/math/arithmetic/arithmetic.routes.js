360903287c90962a18764f08060ea86303f323dd
jigth <danielochoamontes@gmail.com>
2024-06-07 19:12:55 -0500

    REFACTOR: Extract route handlers to separate functions

--- src/modules/math/arithmetic/arithmetic.routes.js
+++ src/modules/math/arithmetic/arithmetic.routes.js
@@ -72,160 +72,173 @@ const validateIntNumberInput = (req) => {
     }
 }
 
+const handleSum = (req, res) => {
+    const { inputs, errors } = validateArithmeticInputs(req)
+
+    if (errors.length > 0) {
+        res.status(errors[0].statusCode).send({
+            msg: "Invalid input, please check the following errors",
+            errors,
+        })
+        return
+    }
+
+    const { a, b } = inputs
+    const result = Arithmetic.sum(a, b)
+    
+    res.status(200).send({
+        result
+    })
+}
+
+const handleMult = (req, res) => {
+    const { inputs, errors } = validateArithmeticInputs(req)
+
+    if (errors.length > 0) {
+        res.status(errors[0].statusCode).send({
+            msg: "Invalid input, please check the following errors",
+            errors,
+        })
+        return
+    }
+
+    const { a, b } = inputs
+
+    const result = Arithmetic.mult(a, b)
+    res.status(200).send({
+        result
+    })
+}
+
+const handleSub = (req, res) => {
+    const { inputs, errors } = validateArithmeticInputs(req)
+
+    if (errors.length > 0) {
+        res.status(errors[0].statusCode).send({
+            msg: "Invalid input, please check the following errors",
+            errors,
+        })
+        return
+    }
+
+    const { a, b } = inputs
+
+    const result = Arithmetic.sub(a, b)
+    res.status(200).send({
+        result
+    })
+}
+
+const handleDiv = (req, res) => {
+
+    const { inputs, errors } = validateArithmeticInputs(req, true)
+
+    if (errors.length > 0) {
+        res.status(errors[0].statusCode).send({
+            msg: "Invalid input, please check the following errors",
+            errors,
+        })
+        return
+    }
+
+    const { a, b } = inputs
+
+    const result = Arithmetic.div(a, b)
+    res.status(200).send({
+        result
+    })
+}
+
+const handleFactorial = (req, res) => {
+    const { n, error } = validateIntNumberInput(req)
+
+    if (error !== null) {
+        req.status(error.statusCode).send({ 
+            msg: "Error while validating inputs",
+            error: error.msg,
+            type: error.type,
+        })
+        return
+    }
+    
+    try {
+        const result = Arithmetic.fact(n)
+        res.status(200).send({
+            result
+        })
+    } catch (err) {
+        if (err.message.includes('Cannot calculate factorial')) {
+            res.status(400).send({
+                msg: "Out of range input",
+                error: err.message
+            })
+
+            return
+        }
+    }
+}
+
+const handleFibonacci = (req, res) => {
+
+    const { n, error } = validateIntNumberInput(req)
+
+    if (error !== null) {
+        req.status(error.statusCode).send({ 
+            msg: "Error while validating inputs",
+            error: error.msg,
+            type: error.type,
+        })
+        return
+    }
+
+    try {
+
+        const result = Arithmetic.fib(n)
+        res.status(200).send({
+            result
+        })
+
+    } catch (err) {
+        
+        if (err.message.includes('Cannot calculate fibonacci')) {
+            res.status(400).send({
+                msg: "Out of range input",
+                error: err.message
+            })
+
+            return
+        }
+
+        res.status(500).send({
+            msg: "Server error",
+            err
+        })
+        
+    }
+}
+
 const getArithmeticRouter = (mountPath) => {
     const router = Router()
 
     // Basic Arithmetic Operations
-    router.get(`${mountPath}/sum`, (req, res) => {
+    router.get(`${mountPath}/sum`, handleSum)
+    router.get(`${mountPath}/mult`, handleMult)
+    router.get(`${mountPath}/sub`, handleSub)
+    router.get(`${mountPath}/div`, handleDiv)
 
-        const { inputs, errors } = validateArithmeticInputs(req)
-
-        if (errors.length > 0) {
-            res.status(errors[0].statusCode).send({
-                msg: "Invalid input, please check the following errors",
-                errors,
-            })
-            return
-        }
-
-        const { a, b } = inputs
-        const result = Arithmetic.sum(a, b)
-        
-        res.status(200).send({
-            result
-        })
-    })
-
-    router.get(`${mountPath}/mult`, (req, res) => {
-
-        const { inputs, errors } = validateArithmeticInputs(req)
-
-        if (errors.length > 0) {
-            res.status(errors[0].statusCode).send({
-                msg: "Invalid input, please check the following errors",
-                errors,
-            })
-            return
-        }
-
-        const { a, b } = inputs
-
-        const result = Arithmetic.mult(a, b)
-        res.status(200).send({
-            result
-        })
-    })
-
-    router.get(`${mountPath}/sub`, (req, res) => {
-
-        const { inputs, errors } = validateArithmeticInputs(req)
-
-        if (errors.length > 0) {
-            res.status(errors[0].statusCode).send({
-                msg: "Invalid input, please check the following errors",
-                errors,
-            })
-            return
-        }
-
-        const { a, b } = inputs
-
-        const result = Arithmetic.sub(a, b)
-        res.status(200).send({
-            result
-        })
-    })
-
-    router.get(`${mountPath}/div`, (req, res) => {
-
-        const { inputs, errors } = validateArithmeticInputs(req, true)
-
-        if (errors.length > 0) {
-            res.status(errors[0].statusCode).send({
-                msg: "Invalid input, please check the following errors",
-                errors,
-            })
-            return
-        }
-
-        const { a, b } = inputs
-
-        const result = Arithmetic.div(a, b)
-        res.status(200).send({
-            result
-        })
-    })
     // Mathematic (more complex) Arithmetic Operations
-    router.get(`${mountPath}/factorial`, (req, res) => {
-
-        const { n, error } = validateIntNumberInput(req)
-
-        if (error !== null) {
-            req.status(error.statusCode).send({ 
-                msg: "Error while validating inputs",
-                error: error.msg,
-                type: error.type,
-            })
-            return
-        }
-        
-        try {
-            const result = Arithmetic.fact(n)
-            res.status(200).send({
-                result
-            })
-        } catch (err) {
-            if (err.message.includes('Cannot calculate factorial')) {
-                res.status(400).send({
-                    msg: "Out of range input",
-                    error: err.message
-                })
-
-                return
-            }
-        }
-    })
-
-    router.get(`${mountPath}/fibonacci`, (req, res) => {
-
-        const { n, error } = validateIntNumberInput(req)
-
-        if (error !== null) {
-            req.status(error.statusCode).send({ 
-                msg: "Error while validating inputs",
-                error: error.msg,
-                type: error.type,
-            })
-            return
-        }
-
-        try {
-            const result = Arithmetic.fib(n)
-            res.status(200).send({
-                result
-            })
-        } catch (err) {
-
-            if (err.message.includes('Cannot calculate fibonacci')) {
-                res.status(400).send({
-                    msg: "Out of range input",
-                    error: err.message
-                })
-
-                return
-            }
-
-            res.status(500).send({
-                msg: "Server error",
-                err
-            })
-        }
-
-    })
+    router.get(`${mountPath}/factorial`, handleFactorial)
+    router.get(`${mountPath}/fibonacci`, handleFibonacci)
 
     return router
 }
 
 module.exports = {
     getArithmeticRouter,
+    handleSum,
+    handleMult,
+    handleSub,
+    handleDiv,
+    handleFactorial,
+    handleFibonacci,
 }
