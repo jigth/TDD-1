b4c9caf7e452544416a51a87d713d463b2f59b2b
jigth <danielochoamontes@gmail.com>
2024-06-10 14:46:24 -0500

    REFACTOR: Simplify tests structure by reducing indentation levels

--- src/modules/math/arithmetic/arithmetic.routes.spec.js
+++ src/modules/math/arithmetic/arithmetic.routes.spec.js
@@ -1,140 +1,136 @@
 const { handleSum, handleMult, handleDiv, handleSub } = require("./arithmetic.routes")
 
-describe("Arithmetic routes", () => {
+let res
 
-    let res
+beforeEach(() => {
+    res = { status: (code) => ({ send: (result) => result }) }
+})
 
-    beforeEach(() => {
-        res = { status: (code) => ({ send: (result) => result }) }
+describe("Sum", () => {
+    it("should get { result: 15 } when summing a=13 + b=2", () => {
+        const expectedObject = { result: 15 }
+        
+        const req = { query: { a: 13, b: 2 } }
+        
+        const result = handleSum(req, res)
+        expect(result).toEqual(expectedObject)
     })
 
-    describe("Sum", () => {
-        it("should get { result: 15 } when summing a=13 + b=2", () => {
-            const expectedObject = { result: 15 }
-            
-            const req = { query: { a: 13, b: 2 } }
-            
-            const result = handleSum(req, res)
-            expect(result).toEqual(expectedObject)
-        })
-    
-        it("should get { result: -5 } when summing a=10 + b=(-15)", () => {
-            const expectedResponse = { result: -5 }
-            
-            const req = { query: { a: 10, b: -15 } }
+    it("should get { result: -5 } when summing a=10 + b=(-15)", () => {
+        const expectedResponse = { result: -5 }
+        
+        const req = { query: { a: 10, b: -15 } }
 
-            const result = handleSum(req, res)
-            expect(result).toEqual(expectedResponse)
-        })
-    
-        it('should return { msg: "...", errors: [{ ...props }, { ...props }]} when not passed enough arguments', () => {
-    
-            const result = handleSum({ query: { a: 13 } }, res)
-            expect(result).toHaveProperty('msg')
-            expect(result).toHaveProperty('errors')
-            
-            const result2 = handleSum({ query: { b: 25 } }, res)
-            expect(result2).toHaveProperty('msg')
-            expect(result2).toHaveProperty('errors')
-        })
-    
-        it('should return { msg: "...", errors: [{ ...props }, { ...props }]} when passed arguments of invalid type', () => {
-    
-            const result = handleSum({ query: { a: "I am not a number", b: "Me neither" } }, res)
-            expect(result).toHaveProperty('msg')
-            expect(result).toHaveProperty('errors')
-        })
+        const result = handleSum(req, res)
+        expect(result).toEqual(expectedResponse)
     })
 
-    describe("Mult", () => {
-        it("should get { result: 10 } when multiplying a=5 * b=2", () => {
-            const expectedObject = { result: 10 }
-            
-            const req = { query: { a: 5, b: 2 } }
-            
-            const result = handleMult(req, res)
-            expect(result).toEqual(expectedObject)
-        })
-    
-        it("should get { result: -15 } when multiplying a=(-3) * b=5", () => {
-            const expectedResponse = { result: -15 }
-            
-            const req = { query: { a: -3, b: 5 } }
+    it('should return { msg: "...", errors: [{ ...props }, { ...props }]} when not passed enough arguments', () => {
 
-            const result = handleMult(req, res)
-            expect(result).toEqual(expectedResponse)
-        })
-    
-        it('should return { msg: "...", errors: [{ ...props }, { ...props }]} when passed arguments of invalid type', () => {
-    
-            const result = handleMult({ query: { a: "I am not a number", b: "Me neither" } }, res)
-            expect(result).toHaveProperty('msg')
-            expect(result).toHaveProperty('errors')
-        })
+        const result = handleSum({ query: { a: 13 } }, res)
+        expect(result).toHaveProperty('msg')
+        expect(result).toHaveProperty('errors')
+        
+        const result2 = handleSum({ query: { b: 25 } }, res)
+        expect(result2).toHaveProperty('msg')
+        expect(result2).toHaveProperty('errors')
     })
 
-    describe("Sub", () => {
-        it("should get { result: 11 } when substracting a=20 - b=9", () => {
-            const expectedObject = { result: 11 }
-            
-            const req = { query: { a: 20, b: 9 } }
-            
-            const result = handleSub(req, res)
-            expect(result).toEqual(expectedObject)
-        })
-    
-        it("should get { result: -5 } when substracting a=10 - b=15", () => {
-            const expectedResponse = { result: -5 }
-            
-            const req = { query: { a: 10, b: 15 } }
+    it('should return { msg: "...", errors: [{ ...props }, { ...props }]} when passed arguments of invalid type', () => {
 
-            const result = handleSub(req, res)
-            expect(result).toEqual(expectedResponse)
-        })
-    
-        it('should return { msg: "...", errors: [{ ...props }, { ...props }]} when passed arguments of invalid type', () => {
-    
-            const result = handleSum({ query: { a: "I am not a number", b: "Me neither" } }, res)
-            expect(result).toHaveProperty('msg')
-            expect(result).toHaveProperty('errors')
-        })
+        const result = handleSum({ query: { a: "I am not a number", b: "Me neither" } }, res)
+        expect(result).toHaveProperty('msg')
+        expect(result).toHaveProperty('errors')
+    })
+})
+
+describe("Mult", () => {
+    it("should get { result: 10 } when multiplying a=5 * b=2", () => {
+        const expectedObject = { result: 10 }
+        
+        const req = { query: { a: 5, b: 2 } }
+        
+        const result = handleMult(req, res)
+        expect(result).toEqual(expectedObject)
     })
 
-    describe("Div", () => {
-        it('should get { result: 3 } when dividing a=6 / b=2', () => {
-            const expectedObject = { result: 3 }
-            
-            const req = { query: { a: 6, b: 2 } }
-            
-            const result = handleDiv(req, res)
-            expect(result).toEqual(expectedObject)
-        })
-    
-        it('should get { result: -6 } when multiplying a=(-3) + b=5', () => {
-            const expectedResponse = { result: -6 }
-            
-            const req = { query: { a: -30, b: 5 } }
+    it("should get { result: -15 } when multiplying a=(-3) * b=5", () => {
+        const expectedResponse = { result: -15 }
+        
+        const req = { query: { a: -3, b: 5 } }
 
-            const result = handleDiv(req, res)
-            expect(result).toEqual(expectedResponse)
-        })
-    
-        it('should get "zeroDivisionError" error when dividing by zero', () => {
-            const expectedErrorType = "zeroDivisionError"
-
-            const req = { query: { a: 50, b: 0 } };
-            
-            const result = handleDiv(req, res)
-            expect(result).toHaveProperty('errors')
-
-            expect(result.errors[0].type).toBe(expectedErrorType)
-        })
-
-        it('should return { msg: "...", errors: [{ ...props }, { ...props }]} when passed arguments of invalid type', () => {
-            const result = handleMult({ query: { a: "I am not a number", b: "Me neither" } }, res)
-            expect(result).toHaveProperty('msg')
-            expect(result).toHaveProperty('errors')
-        })
+        const result = handleMult(req, res)
+        expect(result).toEqual(expectedResponse)
     })
 
+    it('should return { msg: "...", errors: [{ ...props }, { ...props }]} when passed arguments of invalid type', () => {
+
+        const result = handleMult({ query: { a: "I am not a number", b: "Me neither" } }, res)
+        expect(result).toHaveProperty('msg')
+        expect(result).toHaveProperty('errors')
+    })
+})
+
+describe("Sub", () => {
+    it("should get { result: 11 } when substracting a=20 - b=9", () => {
+        const expectedObject = { result: 11 }
+        
+        const req = { query: { a: 20, b: 9 } }
+        
+        const result = handleSub(req, res)
+        expect(result).toEqual(expectedObject)
+    })
+
+    it("should get { result: -5 } when substracting a=10 - b=15", () => {
+        const expectedResponse = { result: -5 }
+        
+        const req = { query: { a: 10, b: 15 } }
+
+        const result = handleSub(req, res)
+        expect(result).toEqual(expectedResponse)
+    })
+
+    it('should return { msg: "...", errors: [{ ...props }, { ...props }]} when passed arguments of invalid type', () => {
+
+        const result = handleSum({ query: { a: "I am not a number", b: "Me neither" } }, res)
+        expect(result).toHaveProperty('msg')
+        expect(result).toHaveProperty('errors')
+    })
+})
+
+describe("Div", () => {
+    it('should get { result: 3 } when dividing a=6 / b=2', () => {
+        const expectedObject = { result: 3 }
+        
+        const req = { query: { a: 6, b: 2 } }
+        
+        const result = handleDiv(req, res)
+        expect(result).toEqual(expectedObject)
+    })
+
+    it('should get { result: -6 } when multiplying a=(-3) + b=5', () => {
+        const expectedResponse = { result: -6 }
+        
+        const req = { query: { a: -30, b: 5 } }
+
+        const result = handleDiv(req, res)
+        expect(result).toEqual(expectedResponse)
+    })
+
+    it('should get "zeroDivisionError" error when dividing by zero', () => {
+        const expectedErrorType = "zeroDivisionError"
+
+        const req = { query: { a: 50, b: 0 } };
+        
+        const result = handleDiv(req, res)
+        expect(result).toHaveProperty('errors')
+
+        expect(result.errors[0].type).toBe(expectedErrorType)
+    })
+
+    it('should return { msg: "...", errors: [{ ...props }, { ...props }]} when passed arguments of invalid type', () => {
+        const result = handleMult({ query: { a: "I am not a number", b: "Me neither" } }, res)
+        expect(result).toHaveProperty('msg')
+        expect(result).toHaveProperty('errors')
+    })
 })
--- src/modules/math/arithmetic/arithmetic.spec.js
+++ src/modules/math/arithmetic/arithmetic.spec.js
@@ -1,147 +1,143 @@
 const { Arithmetic } = require('./arithmetic.js')
 
-describe('Arithmetic', () => {
-
-    describe('Sum', () => {
-        it('sums 2+2 and get 4', () => {
-            expect(Arithmetic.sum(2, 2)).toBe(4)
-        })
-
-        it('sums 4+9 and get 13', () => {
-            expect(Arithmetic.sum(4, 9)).toBe(13)
-        })
-
-        it('sums 0+0 and get 0', () => {
-            expect(Arithmetic.sum(0, 0)).toBe(0)
-        })
-
-        it('sums 10+(-5) and get 5', () => {
-            expect(Arithmetic.sum(10, -5)).toBe(5)
-        })
+describe('Sum', () => {
+    it('sums 2+2 and get 4', () => {
+        expect(Arithmetic.sum(2, 2)).toBe(4)
     })
 
-    describe('Multiplication', () => {
-        it('multiplies 2*5 and get 10', () => {
-            expect(Arithmetic.mult(2, 5)).toBe(10)
-        })
-
-        it('multiplies 5*2 and get 10', () => {
-            expect(Arithmetic.mult(5, 2)).toBe(10)
-        })
-
-        it('multiplies 7*5 and get 35', () => {
-            expect(Arithmetic.mult(7, 5)).toBe(35)
-        })
-
-        it('multiplies 20*0 and get 0', () => {
-            expect(Arithmetic.mult(20, 0)).toBe(0)
-        })
-
-        it('multiplies 20*(-1) and get -20', () => {
-            expect(Arithmetic.mult(20, -1)).toBe(-20)
-        })
-
-        it('multiplies (-20)*20 and get -400', () => {
-            expect(Arithmetic.mult(-20, 20)).toBe(-400)
-        })
-
-        it('multiplies (-20)*(-20) and get 400', () => {
-            expect(Arithmetic.mult(-20, -20)).toBe(400)
-        })
+    it('sums 4+9 and get 13', () => {
+        expect(Arithmetic.sum(4, 9)).toBe(13)
     })
 
-    describe('Substraction', () => {
-        it('substracts 10-5 and gets 5', () => {
-            expect(Arithmetic.sub(10, 5)).toBe(5)
-        })
-        
-        it('substracts 10-10 and gets 0', () => {
-            expect(Arithmetic.sub(10, 10)).toBe(0)
-        })
-
-        it('substracts 1000-10 and gets 990', () => {
-            expect(Arithmetic.sub(1000, 10)).toBe(990)
-        })
-
-        it('substracts 0-0 and gets 0', () => {
-            expect(Arithmetic.sub(0, 0)).toBe(0)
-        })
-
-        it('substracts 5-(-5) and gets 10', () => {
-            expect(Arithmetic.sub(5, -5)).toBe(10)
-        })
-
-        it('substracts -5-(-5) and gets 0', () => {
-            expect(Arithmetic.sub(-5, -5)).toBe(0)
-        })
+    it('sums 0+0 and get 0', () => {
+        expect(Arithmetic.sum(0, 0)).toBe(0)
     })
 
-    describe('Division', () => {
-        it('divides 5/5 and gets 1', () => {
-            expect(Arithmetic.div(5, 5)).toBe(1)
-        })
+    it('sums 10+(-5) and get 5', () => {
+        expect(Arithmetic.sum(10, -5)).toBe(5)
+    })
+})
 
-        it('divides 35/5 and gets 7', () => {
-            expect(Arithmetic.div(35, 5)).toBe(7)
-        })
-
-        it('divides 50000/5 and gets 10000', () => {
-            expect(Arithmetic.div(50000, 5)).toBe(10000)
-        })
-
-        it('divides 50000/-5 and gets -10000', () => {
-            expect(Arithmetic.div(50000, -5)).toBe(-10000)
-        })
-
-        it('divides -50000/10 and gets -5000', () => {
-            expect(Arithmetic.div(-50000, 10)).toBe(-5000)
-        })
-
-        it('divides -50000/-10 and gets 5000', () => {
-            expect(Arithmetic.div(-50000, -10)).toBe(5000)
-        })
+describe('Multiplication', () => {
+    it('multiplies 2*5 and get 10', () => {
+        expect(Arithmetic.mult(2, 5)).toBe(10)
     })
 
-    describe('Factorial', () => {
-        it('calculates fact(5) and gets 120', () => {
-            expect(Arithmetic.fact(5)).toBe(120)
-        })
-
-        it('calculates fact(2) and gets 2', () => {
-            expect(Arithmetic.fact(2)).toBe(2)
-        })
-
-        it('calculates fact(0) and gets 1', () => {
-            expect(Arithmetic.fact(0)).toBe(1)
-        })
-
-        it('calculates fact(7) and gets 5040', () => {
-            expect(Arithmetic.fact(7)).toBe(5040)
-        })
-
-        // TODO: Add error test here, I didn't add it for now because of lack of internet connection.
+    it('multiplies 5*2 and get 10', () => {
+        expect(Arithmetic.mult(5, 2)).toBe(10)
     })
 
-    describe('Fibonacci', () => {
-        it('calculates fib(0) and gets 0', () => {
-            expect(Arithmetic.fib(0)).toBe(0)
-        })
-
-        it('calculates fib(1) and gets 1', () => {
-            expect(Arithmetic.fib(1)).toBe(1)
-        })
-
-        it('calculates fib(2) and gets 2', () => {
-            expect(Arithmetic.fib(2)).toBe(1)
-        })
-
-        it('calculates fib(4) and gets 5', () => {
-            expect(Arithmetic.fib(4)).toBe(3)
-        })
-
-        it('calculates fib(7) and gets 13', () => {
-            expect(Arithmetic.fib(7)).toBe(13)
-        })
+    it('multiplies 7*5 and get 35', () => {
+        expect(Arithmetic.mult(7, 5)).toBe(35)
     })
 
+    it('multiplies 20*0 and get 0', () => {
+        expect(Arithmetic.mult(20, 0)).toBe(0)
+    })
+
+    it('multiplies 20*(-1) and get -20', () => {
+        expect(Arithmetic.mult(20, -1)).toBe(-20)
+    })
+
+    it('multiplies (-20)*20 and get -400', () => {
+        expect(Arithmetic.mult(-20, 20)).toBe(-400)
+    })
+
+    it('multiplies (-20)*(-20) and get 400', () => {
+        expect(Arithmetic.mult(-20, -20)).toBe(400)
+    })
+})
+
+describe('Substraction', () => {
+    it('substracts 10-5 and gets 5', () => {
+        expect(Arithmetic.sub(10, 5)).toBe(5)
+    })
+    
+    it('substracts 10-10 and gets 0', () => {
+        expect(Arithmetic.sub(10, 10)).toBe(0)
+    })
+
+    it('substracts 1000-10 and gets 990', () => {
+        expect(Arithmetic.sub(1000, 10)).toBe(990)
+    })
+
+    it('substracts 0-0 and gets 0', () => {
+        expect(Arithmetic.sub(0, 0)).toBe(0)
+    })
+
+    it('substracts 5-(-5) and gets 10', () => {
+        expect(Arithmetic.sub(5, -5)).toBe(10)
+    })
+
+    it('substracts -5-(-5) and gets 0', () => {
+        expect(Arithmetic.sub(-5, -5)).toBe(0)
+    })
+})
+
+describe('Division', () => {
+    it('divides 5/5 and gets 1', () => {
+        expect(Arithmetic.div(5, 5)).toBe(1)
+    })
+
+    it('divides 35/5 and gets 7', () => {
+        expect(Arithmetic.div(35, 5)).toBe(7)
+    })
+
+    it('divides 50000/5 and gets 10000', () => {
+        expect(Arithmetic.div(50000, 5)).toBe(10000)
+    })
+
+    it('divides 50000/-5 and gets -10000', () => {
+        expect(Arithmetic.div(50000, -5)).toBe(-10000)
+    })
+
+    it('divides -50000/10 and gets -5000', () => {
+        expect(Arithmetic.div(-50000, 10)).toBe(-5000)
+    })
+
+    it('divides -50000/-10 and gets 5000', () => {
+        expect(Arithmetic.div(-50000, -10)).toBe(5000)
+    })
+})
+
+describe('Factorial', () => {
+    it('calculates fact(5) and gets 120', () => {
+        expect(Arithmetic.fact(5)).toBe(120)
+    })
+
+    it('calculates fact(2) and gets 2', () => {
+        expect(Arithmetic.fact(2)).toBe(2)
+    })
+
+    it('calculates fact(0) and gets 1', () => {
+        expect(Arithmetic.fact(0)).toBe(1)
+    })
+
+    it('calculates fact(7) and gets 5040', () => {
+        expect(Arithmetic.fact(7)).toBe(5040)
+    })
+
+    // TODO: Add error test here, I didn't add it for now because of lack of internet connection.
+})
+
+describe('Fibonacci', () => {
+    it('calculates fib(0) and gets 0', () => {
+        expect(Arithmetic.fib(0)).toBe(0)
+    })
+
+    it('calculates fib(1) and gets 1', () => {
+        expect(Arithmetic.fib(1)).toBe(1)
+    })
+
+    it('calculates fib(2) and gets 2', () => {
+        expect(Arithmetic.fib(2)).toBe(1)
+    })
+
+    it('calculates fib(4) and gets 5', () => {
+        expect(Arithmetic.fib(4)).toBe(3)
+    })
+
+    it('calculates fib(7) and gets 13', () => {
+        expect(Arithmetic.fib(7)).toBe(13)
+    })
 })
